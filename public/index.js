85a0a23fdb5affbc30c5649231b0cde69dc25e88
Max Goldstein <goldstem@oregonstate.edu>
2021-06-06 17:34:46 -0700

    added search by name

--- public/index.js
+++ public/index.js
@@ -1,29 +1,5 @@
 var allItems = [];
 
-window.addEventListener('DOMContentLoaded', function(){
-    addItemsToArray();
-})
-
-function insertItem(name, rarity, element, ammo, season){
-    var itemContext = {
-        name: name,
-        rarity: rarity,
-        element: element,
-        ammo: ammo,
-        season: season
-    }
-
-    var itemHtml = Handlebars.templates.item(itemContext)
-
-    var container = document.querySelector("twit-container")
-
-    container.insertAdjacentHTML('beforeend', itemHtml);
-}
-
-function addItemsToArray(){
-    
-}
-
 function expandDetails(button){
     var content = button.target.nextElementSibling
     if (content.style.display === "block") {
@@ -33,13 +9,85 @@ function expandDetails(button){
         content.style.display  = "block";
     }
 }
+
+function doSearch() {
+    var query = document.getElementById("search-input").value
+    var container = document.querySelector(".item-container")
+    
+    while(container.lastChild){
+        container.removeChild(container.lastChild)
+    }
+    
+    allItems.forEach(function (item) {
+        if(searchQuery(item, query)){
+            insertItem(item.img, item.name, item.rarity, item.element, item.ammo, item.season)
+        }
+    })
+}
+
+function searchQuery(item, query){
+    if(!query){
+        return true
+    }
+
+    query = query.trim().toLowerCase()
+    return(item.name.toLowerCase().indexOf(query) >= 0)
+}
+
+function insertItem(img, name, rarity, element, ammo, season){
+    var itemContext = {
+        img: img,
+        name: name,
+        rarity: rarity,
+        element: element,
+        ammo: ammo,
+        season: season
+    }
+
+    var itemHtml = Handlebars.templates.item(itemContext)
+
+    var container = document.querySelector(".item-container")
+
+    container.insertAdjacentHTML('beforeend', itemHtml)
+}
+
+function convertItemToObject(itemEle){
+    var item = {}
+
+    var itemImg = itemEle.querySelector('.item-image')
+    item.img = itemImg.src.trim()
+
+    var itemName = itemEle.querySelector(".name-tag")
+    item.name = itemName.textContent.trim()
+
+    var rarityName = itemEle.querySelector(".rarity-tag")
+    item.rarity = rarityName.textContent.trim()
+
+    var elementName = itemEle.querySelector(".element-tag")
+    item.element = elementName.textContent.trim()
+
+    var ammoName = itemEle.querySelector(".ammo-tag")
+    item.ammo = ammoName.textContent.trim()
+
+    var seasonName = itemEle.querySelector(".season-tag")
+    item.season = seasonName.textContent.trim()
+
+    return item
+}
     
 
 window.addEventListener('DOMContentLoaded', function (){
+    var itemCollection = document.getElementsByClassName('item')
+    for(var i = 0; i < itemCollection.length; i++){
+        allItems.push(convertItemToObject(itemCollection[i]))
+    }
+
     var buttons = document.querySelectorAll("button")
     buttons.forEach(button => {
-        button.addEventListener("click", expandDetails)
+        button.addEventListener('click', expandDetails)
     })
+
+    var search = document.getElementById("search-input")
+    search.addEventListener('input', doSearch)
 })
 
-//testing 
